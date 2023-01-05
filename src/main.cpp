a2abb4ece1cba3ce29a4fc461e1d66694497ceac
Young-TW <young20050727@gmail.com>
2023-01-06 01:56:58 +0800

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,40 @@ const char ssid[]="AX10 2.4Ghz"; //WiFi name
 const char pwd[]="0932230966"; //WiFi password
 const char* mqtt_server = "192.168.0.239";
 const char* mqtt_id = "ESP32Client";
-const char* mqtt_subscribe_topic = "home";
+const char* mqtt_subscribe_topic = "home/garage/door";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+void trigger_pin(int pin){
+  digitalWrite(pin, HIGH);
+  sleep(1);
+  digitalWrite(pin, LOW);
+}
+
 void callback(char* topic, byte* payload, unsigned int length){
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
- 
   Serial.print("Message:");
+
+  string output = "";
+
   for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+    output = output + (char)payload[i];
+    //Serial.print((char)payload[i]);
   }
- 
-  Serial.println();
+  Serial.print(output);
   Serial.println("-----------------------");
+
+  if(output == "on"){
+    trigger_pin(15);
+  }else if(output == "off"){
+    trigger_pin(2);
+  }
 }
 
 void wf_connect(){
-  WiFi.begin(ssid,pwd); 
+  WiFi.begin(ssid,pwd);
 
   Serial.print("\nWiFi connecting\n\n");
 
